4afa3dd40a22b6946c3ca9964a0a159c2626f1b6
TAURUS-ESSEN <reitarovea@gmail.com>
2025-03-08 18:38:46 +0100

    Refactoring the code

--- src/index.js
+++ src/index.js
@@ -4,8 +4,6 @@ import  {textHome} from "./home.js";
 import  {textMenu} from "./menu.js";
 import  {textAbout} from "./about.js";
 
-console.log("Hello World from your main file!");
-
 const buttons = document.querySelectorAll("button");
     buttons.forEach(button => {
         button.addEventListener('click', (event) => {
@@ -16,22 +14,16 @@ const buttons = document.querySelectorAll("button");
 const content = document.getElementById("content");
 const div = document.createElement("div");
 content.innerHTML = textHome;	
-content.appendChild(div)
+content.appendChild(div);
 
-    function makeTurn(button) {
-        content.innerHTML = '';
-        console.log(`Нажата кнопка: ${button.name}`);
-        if (button.name === "home") {
-            content.innerHTML = textHome;
-            console.log("Загружаем модуль Home...");
-     
-        } else if (button.name  === "menu") {
-            content.innerHTML = textMenu;
-            console.log("Загружаем модуль Menu...");
-        } else if (button.name  === "about") {
-            content.innerHTML = textAbout;
-            console.log("Загружаем модуль About...");
-        }
+function makeTurn(button) {
+    content.innerHTML = '';
+    if (button.name === "home") {
+        content.innerHTML = textHome;
+    } else if (button.name  === "menu") {
+        content.innerHTML = textMenu;
+    } else if (button.name  === "about") {
+        content.innerHTML = textAbout;
     }
- 
+}
 
